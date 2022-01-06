02a61f979137775aaff061cfe1173c38fec6be99
psb9663 <psb9663@gmail.com>
2022-01-06 17:09:01 +0900

    FEAT: 기본 환경 설정(typescript, nodemon 적용)

---
+++ dist/app.js
@@ -0,0 +1,5 @@
+"use strict";
+function adder(base, value) {
+    return `결과는 : ${base + value}입니다.`;
+}
+console.log(adder(200, 20));
---
+++ src/app.ts
@@ -0,0 +1,4 @@
+function adder(base: number, value: number): string {
+    return `결과는 : ${base + value}입니다.`;
+}
+console.log(adder(200, 20));

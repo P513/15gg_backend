f1dccddceefa64dc35e4e9b01e3ce3058740689a
P513 <psb9663@gmail.com>
2022-01-18 22:55:12 +0900

    FEAT: type 변경

--- src/models/nickname.ts
+++ src/models/nickname.ts
@@ -1,44 +1,43 @@
-import { Table, Column, Model, HasMany, ForeignKey, PrimaryKey, IsUUID, DataType, Unique } from 'sequelize-typescript';
-import { ENUM } from 'sequelize/dist';
+import { Table, Column, Model, HasMany, ForeignKey, PrimaryKey, IsUUID, DataType, Unique, Default } from 'sequelize-typescript';
 import User from './user';
 
 @Table
 export default class Nickname extends Model<Nickname>{
-    @IsUUID(4)
-    @PrimaryKey
-    @Column(DataType.STRING)
-    id!: string;
+  @IsUUID(4)
+  @PrimaryKey
+  @Column(DataType.STRING)
+  id!: string;
 
-    @IsUUID(4)
-    @ForeignKey(() => User)
-    @Column(DataType.STRING)
-    userId!: string;
+  @IsUUID(4)
+  @ForeignKey(() => User)
+  @Column(DataType.STRING)
+  userId!: string;
 
-    @Unique(true)
-    @Column(DataType.STRING)
-    name!: string;
+  @Unique(true)
+  @Column(DataType.STRING)
+  name!: string;
 
-    @Column(DataType.STRING)
-    tier!: string;
+  @Column(DataType.STRING)
+  tier!: string;
 
-    @Column(DataType.STRING)
-    rank!: string;
+  @Column(DataType.STRING)
+  rank!: string;
 
-    @Column(DataType.STRING)
-    ment!: string;
+  @Column(DataType.STRING)
+  ment!: string;
 
-    @Column(DataType.ARRAY(ENUM))
-    selfPos!: string;
+  @Column(DataType.ARRAY(DataType.STRING))
+  selfPos!: string;
 
-    @Column(DataType.ARRAY(ENUM))
-    duoPos!: string;
+  @Column(DataType.ARRAY(DataType.STRING))
+  duoPos!: string;
 
-    @Column(DataType.INTEGER)
-    playStyle!: number;
+  @Column(DataType.INTEGER)
+  playStyle!: number;
 
-    @Column(DataType.INTEGER)
-    voice!: number;
+  @Column(DataType.INTEGER)
+  voice!: number;
 
-    @Column(DataType.BOOLEAN)
-    status!: boolean;
+  @Column(DataType.BOOLEAN)
+  status!: boolean;
 }

329454282b625f191c7c6e250982170c75aa0926
P513 <psb9663@gmail.com>
2022-02-02 19:56:37 +0900

    FEAT: 로그인/로그아웃 코드 추가

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,10 +1,9 @@
-import { UserRep } from './../models/index';
 import { NextFunction, Request, Response, Router, } from 'express';
-import bcrypt from 'bcrypt';
-import { isNotLoggedIn } from './middlewares';
-import Nickname from '../models/nickname';
+import { isNotLoggedIn, isLoggedIn } from './middlewares';
 import passport from 'passport';
 import User from '../models/user';
+import * as classes from '../config/classes';
+import * as middlewares from './middlewares';
 export const auth = Router();
 
 auth.get('/', (req: Request, res: Response, next: NextFunction) => {
@@ -14,13 +13,40 @@ auth.get('/', (req: Request, res: Response, next: NextFunction) => {
 auth.post('/signup', isNotLoggedIn, async (req: Request, res: Response, next: NextFunction) => {
   passport.authenticate('signup', async (err: any, _user: User, info: any) => {
     if (err) {
-      return res.status(403).send(err);
+      return res.status(403).json(middlewares.successFalse(err, '', null));
     }
     if (info) {
-      return res.status(403).send(info);
+      return res.status(403).json(middlewares.successFalse(null, info, null));
     }
     if (_user) {
-      return res.status(200).send("회원가입을 완료하였습니다");
+      const user = new classes.UserInfo(_user);
+      return res.status(200).json(middlewares.successTrue('회원가입을 완료하였습니다', user));
     }
   })(req, res, next);
+})
+
+auth.post('/login', isNotLoggedIn, async (req: Request, res: Response, next: NextFunction) => {
+  passport.authenticate('login', (err: any, _user: User, info: any) => {
+    if (err || !_user) {
+      return res.status(403).json(middlewares.successFalse(err, '', null));
+    }
+    if (info) {
+      return res.status(403).json(middlewares.successFalse(null, '아이디나 비밀번호를 확인하세요', null));
+    }
+    return req.login(_user, async (err: any) => {
+      if (err) {
+        console.log(err);
+        return res.status(403).json(middlewares.successFalse(err, '로그인에 실패했습니다', null));
+      }
+      return res.status(200).json(middlewares.successTrue('로그인되었습니다', _user));
+    })
+  })(req, res, next);
+})
+
+auth.get('/logout', isLoggedIn, (req: Request, res: Response) => {
+  req.logout();
+  req.session.destroy(function () {
+    req.session;
+  });
+  return res.status(200).json(middlewares.successTrue('로그아웃되었습니다', null));
 })
